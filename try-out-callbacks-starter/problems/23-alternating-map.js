8c50e024eeab9ad7f8eed60262692f6d65812911
Alexis West <alexishwest98@gmail.com>
2022-07-13 15:34:13 -0700

    problems 22-24 solved

--- try-out-callbacks-starter/problems/23-alternating-map.js
+++ try-out-callbacks-starter/problems/23-alternating-map.js
@@ -36,8 +36,19 @@ console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper))
 *******************************************************************************/
 
 
-let alternatingMap = function() {
+let alternatingMap = function(array, cb1, cb2) {
+    let newArr = []
 
+    for(let i = 0; i < array.length; i++){
+        let el = array[i]
+        if(i % 2 === 0) {
+            newArr.push(cb1(el))
+        }
+        else newArr.push(cb2(el))
+    }
+
+    return newArr;
+    
 };
 
 
